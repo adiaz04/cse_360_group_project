5f783142c57df9fd122ac5804661869b4d519f82
Angel Diaz <adiaz04@yahoo.com>
2016-04-04 11:45:42 -0700

    added dice script to dice.

---
+++ dice_trivia/Assets/Dice.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dice : MonoBehaviour {
+
+    public float torqueAmount = 100000;
+    public float torque;
+    public bool roll = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (roll == true)
+        {
+            Roll();
+        }
+	}
+
+    void Roll()
+    {
+        torque = Random.Range(500, torqueAmount) * (Time.deltaTime * 100);
+        GetComponent<Rigidbody>().AddTorque(transform.up * torque * torqueAmount);
+        GetComponent<Rigidbody>().AddTorque(transform.right * torque * torqueAmount);
+        roll = false;
+    }
+}
