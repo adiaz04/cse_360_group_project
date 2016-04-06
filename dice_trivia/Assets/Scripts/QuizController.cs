82daa5d02aaf9a281f513eaf8bc632133eb08198
DelishusCake <redkast@gmail.com>
2016-04-06 14:25:18 -0700

    Empty classes for Question and QuizController

---
+++ dice_trivia/Assets/Scripts/QuizController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuizController : MonoBehaviour
+{
+	public Question[] questions;
+	// Use this for initialization
+	void Start ()
+	{
+		//Load quiz questions
+		//Parse quiz questions
+	}
+
+	public Question GetQuestion()
+	{
+		return null;
+	};
+}
