b287c1a839c54a2f7d04d0611c28c096db08ce23
Itay Braverman <itaybraverman@gmail.com>
2016-04-10 06:47:54 -0700

    Player and Stats
    - Added save/load top score data
    - Added dice and quiz helper for testing
    - Added player avatar and animation
    - Added all movement rules

--- dice_trivia/Assets/Scripts/LevelManager.cs
+++ dice_trivia/Assets/Scripts/LevelManager.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 public class LevelManager : MonoBehaviour {
     public Transform[] boardSpots;
     public Transform spotTypeRed;
@@ -11,11 +15,38 @@ public class LevelManager : MonoBehaviour {
     public int spotsToMove = 6;
     private int currentPlayerLocation;
 
-	/// <summary>
+
+
+    int PlayerInaRow = 0;
+    int playerLastMove = 0;
+
+    // Current player statistics
+    float playerTotalTime = 0.0f;
+    int playerTotalMoves = 0;
+    int playerTotalTrue = 0;
+    int playerTotalFalse = 0;
+    // Top plater statistics
+    float TopTotalTime = 0.0f;
+    int TopTotalMoves = 0;
+    int TopTotalTrue = 0;
+    int TopTotalFalse = 0;
+
+
+    public GameObject diceMenu;
+    public GameObject quizMenu;
+
+    public GameObject statsInARow;
+    /// <summary>
     /// Initialze the scene.
     /// </summary>
-	void Start () {
-	    for (int index = 0; index < numberOfSpots; index++)
+    void Start ()
+    {
+
+        LoadData(); // Loading top score statistics from file 
+
+        quizMenu.SetActive(false); // Hiding the DEV quiz panel
+
+        for (int index = 0; index < numberOfSpots; index++)
         {
             boardSpots[index] = (Transform)Instantiate(spotTypeBlue, new Vector3(index, 0, 0), Quaternion.identity);
         }
@@ -26,26 +57,210 @@ public class LevelManager : MonoBehaviour {
 	/// <summary>
     /// Called once every frame, used to update the scene.
     /// </summary>
-	void Update () {
+	void Update ()
+    {
+
+        /*
 	    if (move)
         {
             movePlayer(spotsToMove);
         }
-	}
+        */
+
+        playerTotalTime += Time.deltaTime; // Updating the timer
+    }
+
 
     /// <summary>
     /// Move player appropriate number of spaces.
     /// </summary>
     /// <param name="inSpotsToMove"></param>
-    private void movePlayer(int inSpotsToMove)
+    /// 
+
+    // Moved to the Player prefab
+
+
+    // Calculating movment based on the quiz answer and the answers history
+    public void checkAnswer (bool theAnswer)
     {
-        move = false;
-        if ((currentPlayerLocation + inSpotsToMove) < boardSpots.Length)
-            currentPlayerLocation = currentPlayerLocation + inSpotsToMove;
+        if (!theAnswer)
+        {
+            playerTotalFalse += 1; // Incrementing total false answers
+
+            if (PlayerInaRow == -2)
+            {
+                movePlayer((playerLastMove * -1) + 2);
+                PlayerInaRow = 0;
+                diceMenu.SetActive(true);
+            }
+            else {
+                movePlayer(playerLastMove * -1);
+                if (PlayerInaRow > 0)
+                {
+                    PlayerInaRow = -1;
+                }
+                else
+                {
+                    PlayerInaRow -= 1;
+                    diceMenu.SetActive(true);
+                }
+            }
+        }
+        else{
+            playerTotalTrue += 1;
+
+            if (PlayerInaRow == 2)
+            {
+                movePlayer(2);
+                PlayerInaRow = 0;
+            }
+            else{
+                if (PlayerInaRow < 0)
+                {
+                    PlayerInaRow = 1;
+                }
+                else
+                {
+                    PlayerInaRow += 1;
+                }
+            }
+        }
+    }
+
+    public void movePlayer(int inSpotsToMove)
+    {
+
+        if (currentPlayerLocation >= boardSpots.Length && playerTotalTime < TopTotalTime)
+        {
+            SaveData(); // Saving data to file when player reached to the end
+        }
         else
-            currentPlayerLocation = boardSpots.Length - 1;
-        player.MoveToPosition(boardSpots[currentPlayerLocation]);
+        {
+            move = false;
+            if ((currentPlayerLocation + inSpotsToMove) < boardSpots.Length)
+                currentPlayerLocation = currentPlayerLocation + inSpotsToMove;
+            else
+                currentPlayerLocation = boardSpots.Length - 1;
+            player.MoveToPosition(boardSpots[currentPlayerLocation]);
+
+            if (inSpotsToMove > 0)
+            {
+                quizMenu.SetActive(true);
+                diceMenu.SetActive(false);
+            }
+            else {
+                quizMenu.SetActive(false);
+                diceMenu.SetActive(true);
+            }
+        }
         
+    }
+
+    //Recieves the spot's transform
+
+    public void SetLastMove(int diceResult)
+    {
+        playerLastMove = diceResult;
+        movePlayer(diceResult);
+    }
+
+
+    // Grabbing data both for the current menu and top menun display
+    public string GetData(string type, string data)
+    {
+    if (type == "Current")
+        {
+            if (data == "Time")
+            {
+                return playerTotalTime.ToString("F2");
+            }
+            else if (data == "Moves")
+            {
+                return playerTotalTime.ToString();
+            }
+            else if (data == "Correct")
+            {
+                return playerTotalTrue.ToString();
+            }
+            else if (data == "False")
+            {
+                return playerTotalFalse.ToString();
+            }
+            else if (data == "IAR")
+            {
+                return PlayerInaRow.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+        else
+        {
+            if (data == "Time")
+            {
+                return TopTotalTime.ToString("F2");
+            }
+            else if (data == "Moves")
+            {
+                return TopTotalTime.ToString();
+            }
+            else if (data == "Correct")
+            {
+                return TopTotalTrue.ToString();
+            }
+            else if (data == "False")
+            {
+                return TopTotalFalse.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+        
+    }
+
+
+    // Save player's data intofile
+    public void SaveData()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream saveFile = File.Create(Application.dataPath + "/topScore.dat");
+
+        TopScoreData data = new TopScoreData();
+        data.totalTime = playerTotalTime;
+        data.totalMoves = playerTotalMoves;
+        data.totalTrue = playerTotalTrue;
+        data.totalFalse = playerTotalFalse;
+
+        bf.Serialize(saveFile, data);
+        saveFile.Close();
+    }
+
+    // Loads top score stats from file
+    public void LoadData()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream saveFile = File.Open(Application.dataPath + "/topScore.dat", FileMode.Open);
+        TopScoreData data = (TopScoreData)bf.Deserialize(saveFile);
+        saveFile.Close();
+
+        TopTotalTime = data.totalTime;
+        TopTotalMoves = data.totalMoves;
+        TopTotalTrue = data.totalTrue;
+        TopTotalFalse = data.totalFalse;
 
     }
+
+}
+
+
+// TopScoreData class
+[Serializable]
+class TopScoreData{
+    public float totalTime;
+    public int totalMoves;
+    public int totalTrue;
+    public int totalFalse;
 }
--- dice_trivia/Assets/Scripts/Player.cs
+++ dice_trivia/Assets/Scripts/Player.cs
@@ -1,29 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     public Vector3 speed = Vector3.zero;
-    public float smoothTime = 0.3F;
+    public float smoothTime = 0.5F;
     private Transform targetLocation;
 
-	// Use this for initialization
-	void Start () {
+
+    // Added by Itay
+    // Avatar parameters
+    Animator animator;
+    public GameObject playerAvatar;
+
+    // Use this for initialization
+    void Start()
+    {
         // scene init.
-	}
-	
-	/// <summary>
+
+        // Getting the animation component
+        animator = playerAvatar.GetComponent<Animator>();
+    }
+
+    /// <summary>
     /// If the target has changed since the previous frame, move to the target location.
     /// </summary>
-	void Update () {
+    void Update()
+    {
         if (this.targetLocation != null)
+        {
             transform.position = Vector3.SmoothDamp(transform.position, targetLocation.position, ref speed, smoothTime);
-	}
+        }
+
+        if (transform.position != targetLocation.position)
+        {
+            animator.enabled = true;
+            animator.speed = 2.0f;
+            animator.Play("Walk");
+        }
+        else
+        {
+            animator.enabled = true;
+            animator.speed = 2.0f;
+            animator.Play("Idle");
+
+        }
+
+    }
 
     /// <summary>
     /// Sets the target location for the player.
     /// </summary>
     /// <param name="location"></param>
-    public void MoveToPosition (Transform location)
+
+
+
+    public void MoveToPosition(Transform location)
     {
         this.targetLocation = location;
     }
