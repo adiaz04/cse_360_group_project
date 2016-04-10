df44fbd87845fe5a6eaee94ea20716cf18294b96
Angel Diaz <adiaz04@yahoo.com>
2016-04-10 14:55:39 -0700

    added a dice rolling area.

--- dice_trivia/Assets/Scripts/Dice.cs
+++ dice_trivia/Assets/Scripts/Dice.cs
@@ -6,40 +6,94 @@ public class Dice : MonoBehaviour {
     public float torqueAmount = 100000;
     public float torque;
     public bool roll = false;
-    public bool value = false;
-	// Use this for initialization
-	void Start () {
+    public int value = 0;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if (roll == true)
-        {
-            Roll();
-        }
-        if (value)
-        {
-            CalculateValue();
-        }
+    private Transform startingPosition;
+    private bool rolled = false;
 
-	}
-
-    private int CalculateValue()
+    /// <summary>
+    /// Returns the current status of the dice by checking velocity and angular velocity.
+    /// </summary>
+    private bool rolling
     {
-        int value = 0;
-        //float dotForward = Vector3.Dot(transform.forward, Vector3.up);
-        float angle = Vector3.Angle(transform.up, Vector3.up);
-        Debug.Log(angle);
-        return value;
+        get
+        {
+            return !(GetComponent<Rigidbody>().velocity.sqrMagnitude < 0.1f) && !(GetComponent<Rigidbody>().angularVelocity.sqrMagnitude < 0.1f);
+        }
     }
 
-    void Roll()
+    /// <summary>
+    /// Returns the value and resets the dice, preparing for the next roll.
+    /// </summary>
+    public int Value
     {
+        get
+        {
+            rolled = false;
+            resetLocation();
+            return value;
+        }
+    }
+
+	/// <summary>
+    /// Record starting position.
+    /// </summary>
+	void Start () {
+        startingPosition = transform;
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update () {
+        if (!rolling && rolled)
+            CalculateValue();
+	}
+
+    /// <summary>
+    /// Reset dice back to it's starting location.
+    /// </summary>
+    private void resetLocation()
+    {
+        transform.position = startingPosition.position;
+    }
+
+    /// <summary>
+    /// Calculate face up by taking the Dot Product of each vector.
+    /// </summary>
+    private void CalculateValue()
+    {
+        float dotForward = Vector3.Dot(transform.forward, Vector3.up);
+        if (dotForward > 0.99f)
+            value = 2;
+        else if (dotForward < -0.99f)
+            value = 1;
+
+        float dotRight = Vector3.Dot(transform.right, Vector3.up);
+        if (dotRight > 0.99f)
+            value = 6;
+        else if (dotRight < -0.99f)
+            value = 5;
+
+        float dotUp = Vector3.Dot(transform.up, Vector3.up);
+        if (dotUp > 0.99f)
+            value = 3;
+        else if (dotUp < -0.99f)
+            dotUp = 4;
+        Debug.Log(value);
+    }
+
+    /// <summary>
+    /// Roll the dice.
+    /// </summary>
+    public void Roll()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
         torque = Random.Range(500, torqueAmount) * (Time.deltaTime * 100);
         GetComponent<Rigidbody>().rotation = Random.rotation;
         GetComponent<Rigidbody>().AddTorque(transform.up * torque * torqueAmount);
         GetComponent<Rigidbody>().AddTorque(transform.right * torque * torqueAmount);
-        roll = false;
     }
+
+    
 }
--- dice_trivia/Assets/Scripts/LevelManager.cs
+++ dice_trivia/Assets/Scripts/LevelManager.cs
@@ -6,10 +6,48 @@ public class LevelManager : MonoBehaviour {
     public Transform spotTypeRed;
     public Transform spotTypeBlue;
     public Player player;
+
+    public Transform playerCameraLocation;
+    public Transform diceCameraLocation;
+    public Transform diceRolledCameraLocation;
+    public Transform dice1;
+    public Transform dice2;
     public int numberOfSpots = 60;
     public bool move = false;
-    public int spotsToMove = 6;
+    public bool timeToRollDice = false;
+    public int spotsToMove = 0;
     private int currentPlayerLocation;
+    private Camera mainCamera;
+
+    // Camera Movement
+    public float cameraMovement = 0;
+    public float cameraSpeed = 0.8f;
+    private Transform targetLocation;
+    private State state;
+
+    enum State
+    {
+        idle = 0,
+        timeToRollDice,
+        diceRolled,
+    }
+
+    /// <summary>
+    /// Used to keep track of current state.
+    /// </summary>
+    private State CurrentState
+    {
+        get
+        {
+            return state;
+        }
+        set
+        {
+            this.state = value;
+            StateChanged();
+        }
+    }
+
 
 	/// <summary>
     /// Initialze the scene.
@@ -20,7 +58,10 @@ public class LevelManager : MonoBehaviour {
             boardSpots[index] = (Transform)Instantiate(spotTypeBlue, new Vector3(index, 0, 0), Quaternion.identity);
         }
         player.transform.position = boardSpots[0].transform.position;
-        
+        Camera.main.transform.parent = playerCameraLocation;
+        Camera.main.transform.position = playerCameraLocation.position;
+        Camera.main.transform.rotation = playerCameraLocation.transform.rotation;
+        this.state = State.idle;
 	}
 	
 	/// <summary>
@@ -31,7 +72,69 @@ public class LevelManager : MonoBehaviour {
         {
             movePlayer(spotsToMove);
         }
+        else if (timeToRollDice)
+        {
+            CurrentState = State.timeToRollDice;
+            timeToRollDice = false;
+        }
 	}
+
+    /// <summary>
+    /// Update game to match current state.
+    /// </summary>
+    private void StateChanged()
+    {
+        int valueA = 0;
+        int valueB = 0;
+        MoveCamera();
+        switch (state)
+        {
+            case State.idle:
+                break;
+            case State.timeToRollDice:
+                dice1.GetComponent<Dice>().Roll();
+                dice2.GetComponent<Dice>().Roll();
+                //while (valueA == 0 && valueB == 0)
+                //{
+                    valueA = dice1.GetComponent<Dice>().Value;
+                    valueB = dice2.GetComponent<Dice>().Value;
+                //};
+                
+                spotsToMove = valueA + valueB;
+                //CurrentState = State.idle;
+                move = true;
+                break;
+            case State.diceRolled:
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Move Camera to correct location based on current game state.
+    /// </summary>
+    private void MoveCamera ()
+    {
+        if (CurrentState == State.timeToRollDice)
+        {
+            Camera.main.transform.parent = diceCameraLocation;
+            Camera.main.transform.position = diceCameraLocation.position;
+            Camera.main.transform.rotation = diceCameraLocation.transform.rotation;
+        }
+        else if (CurrentState == State.diceRolled)
+        {
+            Camera.main.transform.parent = diceRolledCameraLocation;
+            Camera.main.transform.position = diceRolledCameraLocation.position;
+            Camera.main.transform.rotation = diceRolledCameraLocation.transform.rotation;
+        }
+        else if (CurrentState == State.idle)
+        {
+            Camera.main.transform.parent = playerCameraLocation;
+            Camera.main.transform.position = playerCameraLocation.position;
+            Camera.main.transform.rotation = playerCameraLocation.transform.rotation;
+        }
+    }
 
     /// <summary>
     /// Move player appropriate number of spaces.
@@ -45,7 +148,5 @@ public class LevelManager : MonoBehaviour {
         else
             currentPlayerLocation = boardSpots.Length - 1;
         player.MoveToPosition(boardSpots[currentPlayerLocation]);
-        
-
     }
 }
