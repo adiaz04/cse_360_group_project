7843d280735dff820941701314d6a2a75e1cd735
Angel Diaz <adiaz004@yahoo.com>
2016-04-11 12:34:18 -0700

    added test which will run all unit tests.

---
+++ dice_trivia/Assembly-CSharpTests/Assets/Scripts/LevelManagerTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class LevelManagerTests
+    {
+        [TestMethod()]
+        public void StartGameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void checkAnswerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void movePlayerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RollDiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetLastMoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
