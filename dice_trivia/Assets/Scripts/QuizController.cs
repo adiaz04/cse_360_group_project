441cecb75a3ea6023dddefc9048ab24a180bb716
DelishusCake <redkast@gmail.com>
2016-04-11 14:58:44 -0700

    Fixed issues with loading questions, file is now assigned via inspector.

--- dice_trivia/Assets/Scripts/QuizController.cs
+++ dice_trivia/Assets/Scripts/QuizController.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 
 public class QuizController : MonoBehaviour
 {
-	public List<Question> questions;
+	//This resource must be set to load the questions
+	public TextAsset questionFile;
+
+	private List<Question> questions;
 	// Use this for initialization
 	void Start ()
 	{
 		//Load quiz questions
-		TextAsset asset = Resources.Load ("Text/questions.txt") as TextAsset;
-		if (asset) {
-			string[] lines = asset.text.Split ('\n');
+		if (questionFile) {
+			string[] lines = questionFile.text.Split ('\n');
 
 			int length = lines.GetLength(0);
 			questions = new List<Question>();
@@ -28,6 +30,7 @@ public class QuizController : MonoBehaviour
 				case 'Q':
 				{
 					currentQuestion = new Question(name);
+					questions.Add(currentQuestion);
 				} break;
 				case 'C':
 				{
@@ -42,6 +45,15 @@ public class QuizController : MonoBehaviour
 				default: break; //Do nothing for any other designator
 				}
 			}
+			foreach(Question q in questions)
+			{
+				Debug.Log("Question: " + q.GetName());
+				for(int i = 0; i < q.GetNumberOfAnswers(); i++)
+				{
+					Answer a = q.GetAnswer(i);
+					Debug.Log("Answer: " + a.GetName() + " Correct ? " + a.GetCorrect());
+				}
+			}
 		} else {
 			throw new UnityException ("Couldn't load the Questions.txt file");
 		}
@@ -53,5 +65,4 @@ public class QuizController : MonoBehaviour
 		Question question = questions [index];
 		return question;
 	}
-
 }
