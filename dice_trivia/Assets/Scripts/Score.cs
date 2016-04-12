e1aff3644004d1a4f8345f6dbe31416bfc9ecf64
Jordan Bruno <jordan.bruno@asu.edu>
2016-04-12 14:19:02 -0700

    Created the Score.cs
    Calculates the score based on time and correct and incorrect answers.
    Added a funtion getScore() for anyone that needs it.

---
+++ dice_trivia/Assets/Scripts/Score.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    // Initializing some variables
+    public int score;
+    public float scoreTime;
+    public int correct;
+    public int wrong;
+    public string MenuType = "";
+
+    GameObject gameStats;
+
+
+    void Start()
+    {
+        gameStats = GameObject.Find("Level Manager");
+    }
+
+
+	// Update is called once per frame
+	void Update()
+    {
+        // Setting scoreTime to the time that the game has run 
+        scoreTime += Time.deltaTime;
+
+        // Recieving total correct and wrong from the level manager
+        LevelManager temp = gameStats.GetComponent<LevelManager>();
+        correct = System.Int32.Parse(temp.GetData("Current", "Correct"));
+        wrong = System.Int32.Parse(temp.GetData("Current", "False"));
+
+        // Testing to make sure we are getting the correct values
+        //Debug.Log("Correct = " + correct);
+        //Debug.Log("Wrong = " + wrong);
+
+        // Calculating the total score
+        score = (int)(10000 - (scoreTime + (-correct*100) + (wrong*100)));
+
+        //Debug.Log("Score = " + score);
+    }
+
+    
+    public int getScore()
+    {
+        return score;
+    }
+}
---
+++ dice_trivia/dice_trivia.CSharpTests/Assets/Scripts/ScoreTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    
+    [TestFixture()]
+    public class ScoreTests
+    {
+
+        [Test()]
+        public void getScoreTest()
+        {
+            
+        }
+    }
+}
