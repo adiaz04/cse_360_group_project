e846350025fb30d5c212df98c95d8658faf907b5
Hunter Brady <redkast@gmail.com>
2016-04-15 14:33:06 -0700

    Better, more testable QuizController
    Now with more comments!

--- dice_trivia/Assets/Scripts/QuizController.cs
+++ dice_trivia/Assets/Scripts/QuizController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -13,56 +13,98 @@ public class QuizController : MonoBehaviour
 	{
 		//Load quiz questions
 		if (questionFile) {
-			string[] lines = questionFile.text.Split ('\n');
-
-			int length = lines.GetLength(0);
-			questions = new List<Question>();
-
-			Question currentQuestion = null;
-			for(int lineIndex = 0; lineIndex < length; lineIndex++)
-			{
-				string line = lines[lineIndex];
-				//Format for a line is <Designator><Space><Name>
-				char firstCharacter = line[0]; //Gets the first character "Designator:
-				string name = line.Substring(2); //Gets the rest of the line (Which starts after the designator and space)
-				switch(firstCharacter)
-				{
-				case 'Q':
-				{
-					currentQuestion = new Question(name);
-					questions.Add(currentQuestion);
-				} break;
-				case 'C':
-				{
-					if(currentQuestion != null)
-						currentQuestion.AddAnswer(name, true);
-				} break;
-				case 'A':
-				{
-					if(currentQuestion  != null)
-						currentQuestion.AddAnswer(name, false);
-				} break;
-				default: break; //Do nothing for any other designator
-				}
-			}
-			foreach(Question q in questions)
-			{
-				Debug.Log("Question: " + q.GetName());
-				for(int i = 0; i < q.GetNumberOfAnswers(); i++)
-				{
-					Answer a = q.GetAnswer(i);
-					Debug.Log("Answer: " + a.GetName() + " Correct ? " + a.GetCorrect());
-				}
-			}
+			GenerateQuestions(questionFile.text);
 		} else {
-			throw new UnityException ("Couldn't load the Questions.txt file");
+			throw new UnityException ("Couldn't load the questions file");
 		}
-		//Parse quiz questions
 	}
+	/// <summary>
+	/// Gets a random question
+	/// </summary>
+	/// <returns>Random Question</returns>
 	public Question GetQuestion()
 	{
 		int index = Random.Range(0, questions.Count);
-		Question question = questions [index];
+		Question question = GetQuestion(index);
 		return question;
+	}
+	/// <summary>
+	/// Get a question by index
+	/// </summary>
+	/// <param name="index">The question index to get</param>
+	/// <returns>The question at the index</returns>
+	public Question GetQuestion(int index)
+	{
+		Question result = null;
+		if(index >= 0 && index < questions.Count)
+		{
+			result = questions[index];	
+		}
+		return result;
+	}
+	/// <summary>
+	/// Generates questions from a question file
+	/// </summary>
+	/// <param name="text">Contents of the question file</param>
+	public void GenerateQuestions(string text)
+	{
+		//Initialize the questions list
+		questions = new List<Question>();
+		//The current question, which subsequent answers will be added to
+		Question currentQuestion = null;
+
+		//Split the file by line
+		string[] lines = text.Split('\n');
+		//Get the number of lines
+		int length = lines.GetLength(0);
+		for (int lineIndex = 0; lineIndex < length; lineIndex++)
+		{
+			//The current line
+			string line = lines[lineIndex];
+			//Format for a line is <Designator><Space><Name>
+			char firstCharacter = line[0]; //Gets the first character "Designator:
+			string name = line.Substring(2); //Gets the rest of the line (Which starts after the designator and space)
+			switch (firstCharacter)
+			{
+				//This line is a question
+				case 'Q':
+					{
+						//Set it as the current question
+						currentQuestion = new Question(name);
+						//Add it to the list
+						questions.Add(currentQuestion);
+					}
+					break;
+				//This is a (correct) answer
+				case 'C':
+					{
+						//add it to the current question
+						if (currentQuestion != null)
+							currentQuestion.AddAnswer(name, true);
+					}
+					break;
+				//This is an (incorrect) answer
+				case 'A':
+					{
+						//add it to the current question
+						if (currentQuestion != null)
+							currentQuestion.AddAnswer(name, false);
+					}
+					break;
+				default: break; //Do nothing for any other designator
+			}
+		}
+		/*
+		//Logs all questions and answers for testing purposes
+		foreach (Question q in questions)
+		{
+			Debug.Log("Question: " + q.GetName());
+			for (int i = 0; i < q.GetNumberOfAnswers(); i++)
+			{
+				Answer a = q.GetAnswer(i);
+				Debug.Log("Answer: " + a.GetName() + " Correct ? " + a.GetCorrect());
+			}
+		}
+		*/
 	}
 }
