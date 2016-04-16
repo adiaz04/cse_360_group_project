e6054c813a92085f2d17bd367342c0444e0c6682
Angel Diaz <adiaz004@yahoo.com>
2016-04-15 22:47:24 -0700

    updated unit tests.

--- dice_trivia/Assembly-CSharpTests/Assets/Scripts/QuizController1Tests.cs
+++ dice_trivia/Assembly-CSharpTests/Assets/Scripts/QuizController1Tests.cs
@@ -14,24 +14,24 @@ namespace Tests
         public void GenerateQuestionsTest()
         {
             string text = "# This is a comment\nQ This is a question\nA This is an incorrect answer\nC This is a correct answer";
-            
 
-            QuizController1 test = new QuestionController1();
+
+            QuizController1 test = new QuizController1();
             test.GenerateQuestions(text);
 
             Question testQuestion = test.GetQuestion(0);
             Assert.IsNotNull(testQuestion);
             Assert.AreEqual("This is a question", testQuestion.GetName());
 
-            Answer testAnswer1 = test.GetAnswer(0);
+            Answer testAnswer1 = testQuestion.GetAnswer(0);
             Assert.IsNotNull(testAnswer1);
             Assert.AreEqual("This is an incorrect answer", testAnswer1.GetName());
             Assert.IsFalse(testAnswer1.GetCorrect());
 
-            Answer testAnswer2 = test.GetAnswer(1);
+            Answer testAnswer2 = testQuestion.GetAnswer(1);
             Assert.IsNotNull(testAnswer2);
             Assert.AreEqual("This is a correct answer", testAnswer2.GetName());
-            Assert.IsFalse(testAnswer2.GetCorrect());
+            Assert.IsTrue(testAnswer2.GetCorrect());
         }
     }
 }
