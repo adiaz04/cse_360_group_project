a4d7b313385379c0b3796d8fc7c69a1f9d37dcaa
Itay Braverman <itaybraverman@gmail.com>
2016-04-29 12:29:56 -0700

    Avatar menu added.

---
+++ dice_trivia/Assets/avatar1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class avatar1 : MonoBehaviour {
+
+    GameObject gameStats;
+    int currentAvatar;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameStats = GameObject.Find("statsManager");
+        statsControl temp = gameStats.GetComponent<statsControl>();
+        currentAvatar = temp.sendAvatar();
+        if (currentAvatar == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else {
+            gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
